1e7997cbae6a95be05bde741bae750f51d2e0f55
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2025-02-14 21:12:49 +0000

    added aload of functions for cgx to test
    hope you proud of me :)

--- dist/classes/managers/KazagumoCommandManager.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.KazagumoCommandManager = void 0;
-const forgescript_1 = require("@tryforge/forgescript");
-class KazagumoCommandManager extends forgescript_1.BaseCommandManager {
-    handlerName = 'kazagumoCommands';
-}
-exports.KazagumoCommandManager = KazagumoCommandManager;
---
+++ src/natives/currentTrackInfo.ts
@@ -0,0 +1,24 @@
+import { Arg, ArgType, NativeFunction } from '@tryforge/forgescript'
+import type { BaseChannel, VoiceBasedChannel } from 'discord.js'
+import { ForgeLink } from '@structures/ForgeLink'
+import { KazagumoTrack } from 'kazagumo'
+import { info } from 'console'
+
+export default new NativeFunction({
+    name: '$currentTrackInfo',
+    description: 'Gets info on a track in a specific guild',
+    brackets: true,
+    unwrap: true,
+    args: [
+        Arg.requiredGuild('Guild ID', 'The ID of the guild'),
+    ],
+    output: ArgType.String,
+    execute: async function(ctx, [guild]) {
+        const kazagumo = ctx.client.getExtension(ForgeLink, true).kazagumo
+
+        const player = kazagumo.getPlayer('guild'); 
+if (!player) return this.customError("No player found!");
+
+return this.success(player.queue.current.getRaw());
+    }
+})
